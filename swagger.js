7a493f97297b1f844c00e51ac858fa7a3308a33a
Shirdi Sai <shirdisai@Shirdis-Laptop.local>
2024-10-09 20:50:32 +0530

    Changed index.html for Swagger documentation

--- swagger.js
+++ swagger.js
@@ -1,32 +1,3 @@
-// // swagger.js
-// const swaggerJsdoc = require("swagger-jsdoc");
-// const swaggerUi = require("swagger-ui-express");
-
-// const options = {
-//   definition: {
-//     openapi: "3.0.0",
-//     info: {
-//       title: "Event Management System API",
-//       version: "1.0.0",
-//       description: "API documentation for Event Management System",
-//     },
-//     servers: [
-//       {
-//         url: "http://localhost:5000", // Update this to your deployed server URL
-//       },
-//     ],
-//   },
-//   apis: ["./routes/*.js"], // Path to your API routes
-// };
-
-// const swaggerSpec = swaggerJsdoc(options);
-
-// const swaggerDocs = (app) => {
-//   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-// };
-
-// module.exports = swaggerDocs;
-
 const swaggerAutogen = require("swagger-autogen")();
 
 const doc = {
